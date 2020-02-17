9900adafa8588ba70235353679439e05de4d0220
Jhomiguel <jhomiguelflorianmendez20@gmail.com>
2020-02-16 22:25:18 -0400

    Commit 1

---
+++ ASPEjercicio2/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ASPEjercicio2.Models;
+
+namespace ASPEjercicio2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(new ArrayNum());
+        }
+        [HttpPost]
+            public IActionResult Index(ArrayNum arrayNum)
+            {
+                if (ModelState.IsValid)
+                {
+                    arrayNum.Nmayor = ArrayNum.Omayor(arrayNum.ArrayNums);
+                    arrayNum.Nmenor = ArrayNum.Omenor(arrayNum.ArrayNums);
+                    return View("Mostrar", arrayNum);
+                }
+
+                return View(arrayNum);
+            }
+
+    }
+}
---
+++ ASPEjercicio2/Models/ArrayNum.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPEjercicio2.Models
+{
+    public class ArrayNum
+    {
+        public List<int> ArrayNums { get; set; }
+        public int Nmayor { get; set; }
+        public int Nmenor { get; set; }
+
+        public static int Omayor(IEnumerable<int> ArrayNum)
+        {
+            int mayor = int.MinValue;
+            foreach (int num in ArrayNum)
+                if (num > mayor)
+                    mayor = num;
+            return mayor;
+        }
+
+        public static int Omenor(IEnumerable<int> ArrayNum)
+        {
+            int menor = int.MaxValue;
+            foreach (int num in ArrayNum)
+                if (num < menor)
+                    menor = num;
+            return menor;
+        }
+
+    }
+}
